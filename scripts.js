807947e078a04b99ac24938ffb4d751c042b7bbf
ArielaFontes <arielashomo@gmail.com>
2024-07-15 19:59:47 -0300

    Final

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
-let formulario = document.querySelector(".formulario")
+let formularioComprar = document.querySelector(".formulario-comprar");
+let formularioTestDrive = document.querySelector(".formulario-test-drive");
 
-function aparecerFormulario(){
-    formulario.style.left = "50%"
+function aparecerFormularioComprar() {
+    formularioComprar.style.left = "50%";
+    formularioTestDrive.style.left = "-50%";
 }
 
-function desaparecerFormulario(){
-    formulario.style.left = "-50%"
+function desaparecerFormularioComprar() {
+    formularioComprar.style.left = "-50%";
 }
 
+function aparecerFormularioTestDrive() {
+    formularioTestDrive.style.left = "50%";
+    formularioComprar.style.left = "-50%";
+}
+
+function desaparecerFormularioTestDrive() {
+    formularioTestDrive.style.left = "-50%";
+}
